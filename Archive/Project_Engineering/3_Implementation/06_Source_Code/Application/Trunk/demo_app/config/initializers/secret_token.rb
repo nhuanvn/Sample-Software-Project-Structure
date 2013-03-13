cd64e092feac20f6b009915c256583b63a709959
nhuanvn <nhuanvn@yahoo.com>
2013-03-13 12:01:40 +0700

    Delete source code from the wrong directory

--- Archive/Project_Engineering/3_Implementation/06_Source_Code/Application/Trunk/demo_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'a9c416722146cd21580edbae79a96bd8c7d0de8182e3eefd163095517961b76a7046321a75445a384271f94998fa0887e4adf0f8b46717d0d2ef6862382f09b6'
